d0212f1fcbc912f118c36ed8a9791406931ee2b1
Peggy Newman <peggydnewman@gmail.com>
2016-05-12 14:22:06 +1000

    fix doi datatable link

--- src/main/webapp/js/doi-admin.js
+++ src/main/webapp/js/doi-admin.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+    $(".clickable-row").click(function() {
+        window.document.location = $(this).data('url');
+    });
+
     $('#doi-admin-table').DataTable({
         "aLengthMenu": [[5, 10, 50, -1], [5, 10, 50, "All"]],
         "bProcessing": true,
@@ -8,7 +12,4 @@ $(document).ready(function() {
     $('#doi-admin-table-loading').hide();
     $('#doi-admin-table').show();
 
-    $(".clickable-row").click(function() {
-        window.document.location = $(this).data('url');
-    });
 });
