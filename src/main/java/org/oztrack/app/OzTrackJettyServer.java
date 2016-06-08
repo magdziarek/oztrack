007a8bd681821f8f7caf4997bf28328e09aed00c
Peggy Newman <peggydnewman@gmail.com>
2016-06-08 11:46:22 +1000

    upgrade to jetty9 and use embedded jetty server

--- src/main/java/org/oztrack/app/OzTrackJettyServer.java
+++ src/main/java/org/oztrack/app/OzTrackJettyServer.java
@@ -1,32 +1,45 @@
 package org.oztrack.app;
 
-import java.io.IOException;
-
-import org.eclipse.jetty.http.HttpSchemes;
-import org.eclipse.jetty.io.EndPoint;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import java.io.File;
+import org.eclipse.jetty.http.HttpVersion;
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.webapp.WebAppContext;
+
 
 public class OzTrackJettyServer {
     public static void main(String[] args) throws Exception {
+
         Server server = new Server();
 
-        SelectChannelConnector httpConnector = new SelectChannelConnector();
-        httpConnector.setPort(8181);
-        server.addConnector(httpConnector);
+        File keystoreFile = new File("keystore");
 
-        SelectChannelConnector httpsConnector = new SelectChannelConnector() {
-            @Override
-            public void customize(EndPoint endpoint, Request request) throws IOException {
-                request.setScheme(HttpSchemes.HTTPS);
-                super.customize(endpoint, request);
-            }
-        };
-        httpsConnector.setForwarded(true);
-        httpsConnector.setPort(8553);
-        server.addConnector(httpsConnector);
+        HttpConfiguration http_config = new HttpConfiguration();
+        http_config.setSecureScheme("https");
+        http_config.setSecurePort(9443);
+        http_config.setOutputBufferSize(32768);
+
+        ServerConnector http = new ServerConnector(server,
+                new HttpConnectionFactory(http_config));
+        http.setPort(8181);
+        http.setIdleTimeout(30000);
+
+        SslContextFactory sslContextFactory = new SslContextFactory();
+        sslContextFactory.setKeyStorePath(keystoreFile.getAbsolutePath());
+        sslContextFactory.setKeyStorePassword("123456");
+        sslContextFactory.setKeyManagerPassword("123456");
+
+        HttpConfiguration https_config = new HttpConfiguration(http_config);
+        SecureRequestCustomizer src = new SecureRequestCustomizer();
+        https_config.addCustomizer(src);
+
+        ServerConnector https = new ServerConnector(server,
+                new SslConnectionFactory(sslContextFactory,HttpVersion.HTTP_1_1.asString()),
+                new HttpConnectionFactory(https_config));
+        https.setPort(9443);
+        https.setIdleTimeout(500000);
+
+        server.setConnectors(new Connector[] { http, https });
 
         WebAppContext context = new WebAppContext();
         context.setContextPath("/");
